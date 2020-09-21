d0b7e6b6d5cb4b760716a992d94bbee872df677b
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-09-21 22:09:07 +0700

    Add files via upload

---
+++ tugas30.js
@@ -0,0 +1,8 @@
+function regex(value){
+	var ambilData = value.match(/[es]/g)
+	console.log(ambilData)
+}
+
+regex(
+ "Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat"
+);
